f0ca4f0c815e1edd6f227834f25ff1c7be671355
Rongji Tu <daniriaatuu@gmail.com>
2024-03-14 21:59:14 +0100

    EnjoyELE blog init commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import "@/styles/globals.css";
+import Head from "next/head";
+import React, { useEffect, useState } from "react";
+import { Layout } from "../components";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>EnjoyELE</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
